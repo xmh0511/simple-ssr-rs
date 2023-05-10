a05c6e39c4eae1064df94af95977b2f8da3c0886
xmh0511 <970252187@qq.com>
2023-05-10 17:47:39 +0800

    update

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ fn main() {
    let mut ssr = SSRender::new("0.0.0.0:8080");
    ssr.set_pub_dir_name("assets");  // specify the name of the public assets directory in the current root directory
    ssr.set_tmpl_dir_name("pages");  // specify the name of the template directory in the current root directory
+  // ssr.set_meta_info_collector(|req:&Request|->HashMap<String, Value>{}); // get meta data from the current request for the tempalte
    ssr.register_function("println".to_owned(), |v:&HashMap<String, Value>|{  // register a function that can be used in template files
 	  let r = v.get("value").ok_or("none")?.as_str().ok_or("none")?;
 	  Ok(Value::String(format!("<p>{r}</p><br/>")))
@@ -13,6 +14,18 @@ fn main() {
    ssr_work!(ssr);
 }
 
+````
+
+#### Use built-in function
+````html
+<!-- 
+	abc.html in the common directory
+	<h3>{{context.title}}</h3>
+-->
+<!-- the common directory is in the same level path with the following HTML file -->
+<div>
+	{{ include_file(path="common/abc.html"), context=`{"title":"abc"}` | safe }}
+</div>
 ````
 
 More details about how to use the template engine can be seen on the home page of [Tera](https://tera.netlify.app/docs/).
--- src/lib.rs
+++ src/lib.rs
@@ -1,145 +1,197 @@
 use salvo::prelude::*;
 use salvo::serve_static::StaticDir;
-use tera::{Context, Function, Result, Tera,Filter};
-use std::{collections::HashMap, sync::Arc};
 pub use serde_json::Value;
+use std::{collections::HashMap, sync::Arc};
+use tera::{Context, Filter, Function, Result, Tera};
 pub use tokio::{self};
 
-type TeraFunctionMap = HashMap<String,Arc<dyn Function + 'static>>;
-type TeraFilterMap = HashMap<String,Arc<dyn Filter + 'static>>;
+type TeraFunctionMap = HashMap<String, Arc<dyn Function + 'static>>;
+type TeraFilterMap = HashMap<String, Arc<dyn Filter + 'static>>;
+type MetaInfoCollector =
+    Option<Arc<dyn Fn(&Request) -> HashMap<String, Value> + 'static + Send + Sync>>;
+struct CallableObjectForTera<F: ?Sized>(Arc<F>);
 
-struct CallableObjectForTera<F:?Sized>(Arc<F>);
-
-impl<F:Function + ?Sized> Function for CallableObjectForTera<F>{
+impl<F: Function + ?Sized> Function for CallableObjectForTera<F> {
     fn call(&self, args: &HashMap<String, Value>) -> Result<Value> {
         self.0.call(args)
     }
 }
 
-impl<F:Filter + ?Sized> Filter for CallableObjectForTera<F>{
+impl<F: Filter + ?Sized> Filter for CallableObjectForTera<F> {
     fn filter(&self, value: &Value, args: &HashMap<String, Value>) -> Result<Value> {
         self.0.filter(value, args)
     }
 }
 
-pub struct SSRender{
-	pub_assets_dir_name:String,
-	tmpl_dir_name:String,
-	host:String,
-	tmpl_func_map: TeraFunctionMap,
-	tmpl_filter_map:TeraFilterMap
+pub struct SSRender {
+    pub_assets_dir_name: String,
+    tmpl_dir_name: String,
+    host: String,
+    tmpl_func_map: TeraFunctionMap,
+    tmpl_filter_map: TeraFilterMap,
+    request_meta_info_collector: MetaInfoCollector,
 }
-impl SSRender{
-	pub fn new(host:&str)->Self {
-		Self{
-			pub_assets_dir_name: "public".to_owned(),
-			tmpl_dir_name: "templates".to_owned(),
-			host:host.to_owned(),
-			tmpl_func_map:HashMap::new(),
-			tmpl_filter_map:HashMap::new(),
-		}
+impl SSRender {
+    pub fn new(host: &str) -> Self {
+        Self {
+            pub_assets_dir_name: "public".to_owned(),
+            tmpl_dir_name: "templates".to_owned(),
+            host: host.to_owned(),
+            tmpl_func_map: HashMap::new(),
+            tmpl_filter_map: HashMap::new(),
+            request_meta_info_collector: None,
+        }
+    }
+
+    pub fn host(&self) -> &str {
+        &self.host
+    }
+
+    pub fn set_pub_dir_name(&mut self, path: &str) {
+        self.pub_assets_dir_name = path.to_owned();
+    }
+
+    pub fn set_tmpl_dir_name(&mut self, path: &str) {
+        self.tmpl_dir_name = path.to_owned();
+    }
+
+    pub fn register_function<F: Function + 'static>(&mut self, k: String, f: F) {
+        self.tmpl_func_map.insert(k, Arc::new(f));
+    }
+
+    pub fn rm_registed_function(&mut self, k: String) {
+        self.tmpl_func_map.remove(&k);
+    }
+
+    pub fn registed_function(&self) -> &TeraFunctionMap {
+        &self.tmpl_func_map
+    }
+
+    pub fn pub_dir_name(&self) -> &str {
+        &self.pub_assets_dir_name
+    }
+
+    pub fn tmpl_dir_name(&self) -> &str {
+        &self.tmpl_dir_name
+    }
+
+    pub fn set_meta_info_collector(
+        &mut self,
+        f: impl Fn(&Request) -> HashMap<String, Value> + 'static + Send + Sync,
+    ) {
+        self.request_meta_info_collector = Some(Arc::new(f));
+    }
+
+	pub fn rm_meta_info_collector(& mut self){
+		self.request_meta_info_collector = None;
 	}
 
-	pub fn host(&self)->&str{
-		&self.host
-	}
-
-	pub fn set_pub_dir_name(& mut self,path:&str){
-		self.pub_assets_dir_name = path.to_owned();
-	}
-
-	pub fn set_tmpl_dir_name(& mut self,path:&str){
-		self.tmpl_dir_name =  path.to_owned();
-	}
-
-	pub fn register_function<F:Function + 'static>(& mut self, k:String, f:F){
-		self.tmpl_func_map.insert(k, Arc::new(f));
-	}
-
-	pub fn rm_registed_function(& mut self, k:String){
-		self.tmpl_func_map.remove(&k);
-	}
-
-	pub fn registed_function(&self)-> &TeraFunctionMap{
-		&self.tmpl_func_map
-	}
-
-	pub fn pub_dir_name(&self)->&str{
-		&self.pub_assets_dir_name
-	}
-
-	pub fn tmpl_dir_name(&self)->&str{
-		&self.tmpl_dir_name
-	}
-
-	pub async fn serve(&self){
-		let pub_assets_router = Router::with_path(format!("{}/<**>",self.pub_assets_dir_name)).get(
-			StaticDir::new([&self.pub_assets_dir_name])
-				.with_defaults("index.html")
-				.with_listing(true),
-		);
-		let view_router = Router::with_path("/<**rest_path>").get(ViewHandler::new(format!("{}/**/*",self.tmpl_dir_name),self.tmpl_func_map.clone(),self.tmpl_filter_map.clone()));
-		let router = Router::new().push(pub_assets_router);
-		let router = router.push(view_router);
-		Server::new(TcpListener::bind(&self.host)).serve(router).await
-	}
+    pub async fn serve(&self) {
+        let pub_assets_router = Router::with_path(format!("{}/<**>", self.pub_assets_dir_name))
+            .get(
+                StaticDir::new([&self.pub_assets_dir_name])
+                    .with_defaults("index.html")
+                    .with_listing(true),
+            );
+        let view_router = Router::with_path("/<**rest_path>").get(ViewHandler::new(
+            format!("{}/**/*", self.tmpl_dir_name),
+            self.tmpl_func_map.clone(),
+            self.tmpl_filter_map.clone(),
+            self.request_meta_info_collector.clone(),
+        ));
+        let router = Router::new().push(pub_assets_router);
+        let router = router.push(view_router);
+        Server::new(TcpListener::bind(&self.host))
+            .serve(router)
+            .await
+    }
 }
 
-struct ViewHandler{
-	dir_path:String,
-	tmpl_func_map: TeraFunctionMap,
-	tmpl_filter_map:TeraFilterMap
+struct ViewHandler {
+    dir_path: String,
+    tmpl_func_map: TeraFunctionMap,
+    tmpl_filter_map: TeraFilterMap,
+    request_meta_info_collector: MetaInfoCollector,
 }
-impl ViewHandler{
-	fn new(v:String, tmp_func:TeraFunctionMap,filter_map:TeraFilterMap)->Self{
-		Self{
-			dir_path:v,
-			tmpl_func_map:tmp_func,
-			tmpl_filter_map:filter_map
-		}
-	}
-	fn register_with_tera(&self, tera:& mut Tera){
-		for (k, v) in &self.tmpl_func_map{
-			tera.register_function(k, CallableObjectForTera(Arc::clone(v)));
-		}
-		for (k, v) in &self.tmpl_filter_map{
-			tera.register_filter(k, CallableObjectForTera(Arc::clone(v)));
-		}
-	}
+impl ViewHandler {
+    fn new(
+        v: String,
+        tmp_func: TeraFunctionMap,
+        filter_map: TeraFilterMap,
+        collector: MetaInfoCollector,
+    ) -> Self {
+        Self {
+            dir_path: v,
+            tmpl_func_map: tmp_func,
+            tmpl_filter_map: filter_map,
+            request_meta_info_collector: collector,
+        }
+    }
+    fn register_with_tera(&self, tera: &mut Tera) {
+        for (k, v) in &self.tmpl_func_map {
+            tera.register_function(k, CallableObjectForTera(Arc::clone(v)));
+        }
+        for (k, v) in &self.tmpl_filter_map {
+            tera.register_filter(k, CallableObjectForTera(Arc::clone(v)));
+        }
+    }
+    fn gen_context(&self, req:&Request) -> Context {
+        match self.request_meta_info_collector {
+            Some(ref collect) => {
+				let mut context = Context::new();
+				for (k,val) in collect(req){
+					context.insert(k, &val);
+				}
+				context
+			}
+            None => {
+				Context::default()
+			}
+        }
+    }
 }
 #[handler]
-impl ViewHandler{
-	async fn handle(&self, req: &mut Request, _depot: &mut Depot, res: &mut Response) {
-		let Some(path) = req.param::<String>("**rest_path") else{
+impl ViewHandler {
+    async fn handle(&self, req: &mut Request, _depot: &mut Depot, res: &mut Response) {
+        let Some(path) = req.param::<String>("**rest_path") else{
 			res.set_status_code(StatusCode::BAD_REQUEST);
 			res.render(Text::Plain("invalid request path"));
 			return;
 		};
-		match Tera::new(&self.dir_path) {
-			Ok(mut tera) => {
-				tera.register_function("include_file", generate_include(tera.clone()));
-				tera.register_filter("json_decode", |v:&Value, _args:&HashMap<String, Value>|->Result<Value>{
-					let v = v.as_str().ok_or(tera::Error::msg("value must be a json object string"))?;
-					let v = serde_json::from_str::<Value>(v)?;
-					Ok(v)
-				});
-				self.register_with_tera(& mut tera);
-				match tera.render(if path.is_empty(){"index.html"}else{&path}, &Context::default()) {
-					Ok(s) => {
-						res.render(Text::Html(s));
-					}
-					Err(e) => {
-						res.set_status_code(StatusCode::BAD_REQUEST);
-						res.render(Text::Plain(format!("{e:?}")));
-					}
-				}
-			}
-			Err(e) => {
-				res.set_status_code(StatusCode::BAD_REQUEST);
-				res.render(Text::Plain(format!("{e:?}")));
-			}
-		}
-	}
+        match Tera::new(&self.dir_path) {
+            Ok(mut tera) => {
+                tera.register_function("include_file", generate_include(tera.clone()));
+                tera.register_filter(
+                    "json_decode",
+                    |v: &Value, _args: &HashMap<String, Value>| -> Result<Value> {
+                        let v = v
+                            .as_str()
+                            .ok_or(tera::Error::msg("value must be a json object string"))?;
+                        let v = serde_json::from_str::<Value>(v)?;
+                        Ok(v)
+                    },
+                );
+                self.register_with_tera(&mut tera);
+				let context = self.gen_context(req);
+                match tera.render(
+                    if path.is_empty() { "index.html" } else { &path },
+                    &context,
+                ) {
+                    Ok(s) => {
+                        res.render(Text::Html(s));
+                    }
+                    Err(e) => {
+                        res.set_status_code(StatusCode::BAD_REQUEST);
+                        res.render(Text::Plain(format!("{e:?}")));
+                    }
+                }
+            }
+            Err(e) => {
+                res.set_status_code(StatusCode::BAD_REQUEST);
+                res.render(Text::Plain(format!("{e:?}")));
+            }
+        }
+    }
 }
 
 fn generate_include(tera: Tera) -> impl Function {
@@ -183,13 +235,13 @@ fn generate_include(tera: Tera) -> impl Function {
 
 #[macro_export]
 macro_rules! ssr_work {
-	($e:expr) => {
-		$crate::tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(async {
-			$e.serve().await;
-		});
-	};
+    ($e:expr) => {
+        $crate::tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(async {
+                $e.serve().await;
+            });
+    };
 }
